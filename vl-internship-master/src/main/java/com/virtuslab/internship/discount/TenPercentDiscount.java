6e76d082fa433de9755cc351aeeaf05043b1fc4c
Michał Taczała <michal@MacBook-Pro-Micha.local>
2022-03-10 17:52:21 +0100

    add configuration and fix file(now tests don't fail)

--- vl-internship-master/src/main/java/com/virtuslab/internship/discount/TenPercentDiscount.java
+++ vl-internship-master/src/main/java/com/virtuslab/internship/discount/TenPercentDiscount.java
@@ -19,6 +19,8 @@ public class TenPercentDiscount {
     }
 
     private boolean shouldApply(Receipt receipt) {
-        return receipt.totalPrice().compareTo(BigDecimal.valueOf(50)) < 0;
+
+
+        return receipt.totalPrice().compareTo(BigDecimal.valueOf(50)) > 0;
     }
 }
--- vl-internship-master/src/main/java/com/virtuslab/internship/receipt/ReceiptGenerator.java
+++ vl-internship-master/src/main/java/com/virtuslab/internship/receipt/ReceiptGenerator.java
@@ -1,9 +1,13 @@
 package com.virtuslab.internship.receipt;
 
 import com.virtuslab.internship.basket.Basket;
+import com.virtuslab.internship.product.Product;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ReceiptGenerator {
 
@@ -14,6 +18,10 @@ public class ReceiptGenerator {
 
     private Receipt getReceipt(Basket basket) {
         List<ReceiptEntry> receiptEntries = new ArrayList<>();
+
+        HashSet<Product> uniqueProducts = new HashSet<Product>(basket.getProducts());
+        receiptEntries = uniqueProducts.stream().map(product -> new ReceiptEntry(product, Collections.frequency(basket.getProducts(), product))).collect(Collectors.toList());
+
         return new Receipt(receiptEntries);
     }
 }
